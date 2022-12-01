ec0c6ceebfe9a6062114bd1df0cc8351e51801bb
Thomas FERREIRA <thomas_ferreira_93330@hotmail.fr>
2022-12-01 12:27:32 +0100

    Day1 solution

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,42 +1,21 @@
-import { Delays, greeter } from '../src/main.js';
+import day1 from '../src/day1.js';
 
-describe('greeter function', () => {
-  const name = 'John';
-  let hello: string;
+describe('day1 challenge tests', () => {
+  it('should get 24000', () => {
+    const input = `1000
+      2000
+      3000
 
-  let timeoutSpy: jest.SpyInstance;
+      4000
 
-  // Act before assertions
-  beforeAll(async () => {
-    // Read more about fake timers
-    // http://facebook.github.io/jest/docs/en/timer-mocks.html#content
-    // Jest 27 now uses "modern" implementation of fake timers
-    // https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults
-    // https://github.com/facebook/jest/pull/5171
-    jest.useFakeTimers();
-    timeoutSpy = jest.spyOn(global, 'setTimeout');
+      5000
+      6000
 
-    const p: Promise<string> = greeter(name);
-    jest.runOnlyPendingTimers();
-    hello = await p;
-  });
+      7000
+      8000
+      9000
 
-  // Teardown (cleanup) after assertions
-  afterAll(() => {
-    timeoutSpy.mockRestore();
-  });
-
-  // Assert if setTimeout was called properly
-  it('delays the greeting by 2 seconds', () => {
-    expect(setTimeout).toHaveBeenCalledTimes(1);
-    expect(setTimeout).toHaveBeenLastCalledWith(
-      expect.any(Function),
-      Delays.Long,
-    );
-  });
-
-  // Assert greeter result
-  it('greets a user with `Hello, {name}` message', () => {
-    expect(hello).toBe(`Hello, ${name}`);
+      10000`;
+    expect(day1.getHigherCaloriesCarryingElf(input)).toBe(24000);
   });
 });
